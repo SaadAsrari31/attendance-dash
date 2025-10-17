e4b80e8685844c3b8f521220e729b6c7b111ac7c
Mohamed Saad Asrari <saadbai0304@gmail.com>
2025-10-17 13:09:22 +0530

    attendance updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const sheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQsPyaz03qLXlDsfSsgQX6Ix1xqfFFf0FaB_ku1Eqd1yr2iB6dJJxy8L1GSicg8eb4_1TxAmcmig8n4/pub?output=csv";
+  const sheetUrl = "https://opensheet.elk.sh/2PACX-1vQsPyaz03qLXlDsfSsgQX6Ix1xqfFFf0FaB_ku1Eqd1yr2iB6dJJxy8L1GSicg8eb4_1TxAmcmig8n4/AttendanceData";
+
 // <-- replace with your ID + sheet name
 
   fetch(sheetUrl)
